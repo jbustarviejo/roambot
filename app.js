9313f0ebdbfdb63d27e701a8f18efb2d3961f14b
jbustarviejo <j.bustarviejo@gmail.com>
2016-09-19 12:17:23 +0200

    Base of mail identification

--- app.js
+++ app.js
@@ -39,110 +39,168 @@ MongoClient.connect('mongodb://127.0.0.1:27017/roambot', function(err, db) {
             server.post('/', connector.listen());
         }
 
+        //LUIS recognizer
+        var recognizer = new builder.LuisRecognizer('https://api.projectoxford.ai/luis/v1/application?id=c413b2ef-382c-45bd-8ff0-f76d60e2a821&subscription-key=6d0966209c6e4f6b835ce34492f3e6d9&q=');
+        var dialog = new builder.IntentDialog({ recognizers: [recognizer] });
+        bot.dialog('/', dialog);
+
+        bot.on('contactRelationUpdate', function (message) {
+            if (message.action === 'add') {
+                database.getUser(message.user.id, function(dbUser){
+                    if(!dbUser){
+                        var name = message.user ? message.user.name: null;
+                        name = getFirstName(name);
+                        name = name ? " "+name : null;
+                        database.users.insert({_id: message.user.id ,createdAt: new Date(), userName: name, authorized: false});
+                        var reply = new builder.Message()
+                                .address(message.address)
+                                .text("¡Hola %s! Soy Roambot, un placer conocerte :)", name);
+                        bot.send(reply);
+                    }else{
+                        //if(dbUser.authorized){
+                        var name = message.user ? message.user.name: null;
+                        name = getFirstName(name);
+                        name = name ? " "+name : null;
+                        var reply = new builder.Message()
+                                .address(message.address)
+                                .text("¡Hola de nuevo %s! ;)", getFirstName(name) || '');
+                        bot.send(reply);
+                    }
+                });
+            } else {
+                // delete their data
+            }
+        });
+
 //=========================================================
 // Bots Dialogs
 //=========================================================
 
-    bot.dialog('/', [
-        function (session, args, next) {
-            //console.log(0);
-            if (!session.dbUser) {
-                //If no user stored, get profile
-                debug && console.log(JSON.stringify(session.message));
-                session.beginDialog('/get-profile');
-            } else {
-                next();
-            }
-        },
-        function (session, result) {
-            //console.log("Response"+JSON.stringify(result.response));
-            if(!result.response.ok==true && !result.response.userName){
-                //console.log("nope...");
-                session.endDialog();
-            }else{
-                //console.log("yes!");
-                session.dbUser=result.response.dbUser;
-                session.beginDialog('/get-profile');
-                session.endDialog();
-            }
-        }
-    ]);
-
-    bot.dialog('/get-profile', [
-        function (session, args, next) {
-            var userName=session.message.user.name;
-            database.getUser(userName, function(dbUser){
+        dialog.onBegin(function (session, args, next) {
+            database.getUser(session.message.user.id, function(dbUser){
                 if(!dbUser){
-                    //console.log("1");
-                    session.send("Hola "+getFirstName(userName)+". Para poder acceder a mis servicios es necesario que se confirme tu identidad en el sistema, no tienes que hacer nada, el administrador se pondrá en contacto contigo. ¡gracias!");
-                    database.users.insert({_id: session.message.user.id ,createdAt: new Date(), userName: userName, authorized: false});
-                    session.endDialog(false);
+                    dbUser = {_id: session.message.user.id, createdAt: new Date(), userName: session.message.user.name, authorized: false, welcomed: true};
+                    database.users.insert(dbUser);
+                }
+                if(!dbUser.authorized){
+                    session.replaceDialog('/get-user-auth', dbUser);
                 }else{
-                    //console.log("2");
-                    if(!dbUser.authorized){
-                        next();
-                    }else{
-                        if(!dbUser.welcomed){
-                            //console.log("4");
-                            dbUser.welcomed=true;
-                            dbUser.updateUser();
-                            session.endDialogWithResult({response: {dbUser: dbUser}});
-                            session.send("¡Hola "+getFirstName(session.message.user.name)+"! Por si no me conoces déjame presentarme: Soy RoamBot, el bot de Roaming de Telefónica GRTU. ¿Cómo puedo ayudarte?\n"+
-                                "Puedes preguntarme por el tiempo");
-                            session.endDialog();
-                            //console.log("4.6");
-                        }else{
-                            //console.log("4.2");
-                            session.endDialogWithResult({response: {ok: true, dbUser: dbUser}});
-                        }
-                    }
-                    //console.log("5");
-                    session.endDialog();
+                    session.dbUser=dbUser;
+                    next();
                 }
             });
-        },function (session, answer) {
-            //console.log("3");
-            var answers=["Lo siento, pero no estás confirmado en el sistema aún","Debes esperar a que se te autorice a hablar conmigo","Sé que estás impaciente por hablar conmigo, pero debes esperar...","Espera a que el administrador te identifique...","Aún no hemos confirmado tu identidad, tienes que esperar", "Tienes que esperar a ser identificado por el administrador"];
-            var randomNumber=Math.floor(Math.random()*answers.length);
-            session.send(answers[randomNumber]);
-            session.endDialog();
-        }
-    ]);
+        });
 
-    bot.dialog('/welcome', new builder.IntentDialog()
-        .matches(/^hola/i, function (session) {
-            console.log(1);
-            session.send("Holaaa "+session.dbUser.name);
-        })
-        .onDefault(function (session) {
-            console.log(2);
-            session.send("No te entendí, ¡sólo dime hola!");
-            session.endDialog();
+        bot.dialog('/get-user-auth', [
+            function (session, args, next) {
+                console.log("Args: ", args);
+                if(!session.userData.dbUser && !args){
+                    throw Exception("Error: No dbUser received");
+                }
+                if(args){
+                    session.userData.dbUser=args;
+                }
+                if(!session.dialogData.welcomed){
+                    session.dialogData.welcomed=true;
+                    // call custom prompt
+                    session.beginDialog('/validate-email-dialog',true);
+                }else{
+                    next();
+                }
+            },
+            //Request & validate email
+            function (session, result, next) {
+                console.log("Res1: ",session);
+                if(!session.dialogData.email){
+                    session.dialogData.email=session.message.text.trim().toLowerCase();
+                    session.userData.dbUser.email=session.dialogData.email;
+                    session.userData.dbUser.validationCode=42;
+
+                    // call custom prompt
+                    session.beginDialog('/validate-code-dialog', true);
+                }else{
+                    next();
+                }
+            },
+            function (session, result, next) {
+                console.log("Res2: ",session);
+                session.userData.dbUser.authorized=true;
+                database.updateUser(session.userData.dbUser, function(){
+                    session.endDialogWithResult({result: {ok: true, dbUser: session.userData.dbUser}});    
+                });
+            }
+        ]);
+
+        dialog.onDefault(builder.DialogAction.send("No te he entendido bien, estoy aún aprendiendo a hablar y me cuesta un poco"));
+
+        bot.dialog('/validate-email-dialog', [
+            function (session, args) {
+                if(args===true){
+                    session.send(session, "Para usar mis servicios es necesario confirmar tu identidad, te voy a enviar un código a tu email para que me lo escribas y así te pueda identificar");
+                }
+                // call custom prompt
+                session.beginDialog('/validate-email-prompt', { 
+                    prompt: "Dime tu email corporativo:", 
+                    retryPrompt: "No parece un email corporativo correcto, reintrodúcelo." 
+                });
+            },
+            function (session, results) {
+                // Check their answer
+                if (results.response) {
+                    session.send("¡Así es!");
+                    session.endDialogWithResult();
+                } else{
+                    session.send("Debes introducir un email corporativo válido y este no lo es :(\n");
+                }
+            }
+        ]);
+
+        bot.dialog('/validate-email-prompt', builder.DialogAction.validatedPrompt(builder.PromptType.text, function (response) {
+            var email=response.trim();
+            var re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+            var valid_email = re.test(email);
+            var valid_telefonica_email = email.endsWith("telefonica.es");
+            return valid_email && valid_telefonica_email;
         }));
 
-    //End of dialogs
+        bot.dialog('/validate-code-dialog', [
+            function (session, args) {
+                if(args){
+                    session.send(session, "Genial, ¡un último paso! Voy a enviarte a "+session.userData.dbUser.email+" un código de seguridad para verificar tu identidad. Cuando lo recibas, vuelve y dime el código aquí:");
+                }
+                // call custom prompt
+                session.beginDialog('/validate-code-prompt', { 
+                    prompt: "Dime el código enviado a tu email", 
+                    retryPrompt: "No parece ser el código correcto, revísalo y reintrodúcelo." 
+                });
+            },
+            function (session, results) {
+                // Check their answer
+                if (results.response) {
+                    session.send("Gracias por verificar tu identidad. Es importante estar seguros (cool)");
+                    session.endDialog();
+                } else{
+                    session.send("Debes revisar en tu correo para verificar el código que te ha llegado y decírmelo aquí sin espacios y respetando las mayúsculas");
+                }
+            }
+        ]);
+
+        bot.dialog('/validate-code-prompt', builder.DialogAction.validatedPrompt(builder.PromptType.text, function (response) {
+            if(response=="42"){
+                return true;
+            }
+            return false;
+        }));
+
+        console.log("=>RoamBot ready!");
     });
-    console.log("=>RoamBot ready!");
 });
 
-database.getUser=function(userName, callback){
-    this.users.findOne({userName: userName}, function(err, doc){
+database.getUser=function(id, callback){
+    this.users.findOne({_id: id}, function(err, doc){
         if (doc){
             debug && console.log("user found!...");
-            doc.updateUser=function(){
-                var user=this;
-                //console.log(this);
-                database.users.update({userName: user.userName},{$set:{
-                    updatedAt: new Date(),
-                    userName: user.userName,
-                    welcomed: user.welcomed || false,
-                }});/*}, function(err, doc){
-                    if(err){
-                        console.log("Error updating user: "+err);
-                    }
-                    callback && callback(); return;
-                }*/
-            };
+            doc.updateUser = database.updateUser;
             callback && callback(doc);
         }else{
             debug && console.log("No user found...");
@@ -151,7 +209,23 @@ database.getUser=function(userName, callback){
     }); 
 }
 
+database.updateUser=function(user, callback){
+    console.log("Actualizando...",user);
+    database.users.update({userName: user.userName},{$set:{
+        updatedAt: new Date(),
+        userName: user.userName,
+        welcomed: user.welcomed || false,
+        authorized: user.authorized || false,
+        validationCode: user.validationCode
+    }}, function(){
+        callback && callback();
+    });
+};
+
 getFirstName=function(userName){
+    if(!userName){
+        return null;
+    }
     var names=userName.split(" ");
     return names[0];
 }
